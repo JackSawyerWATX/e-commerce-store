3056a03a5aa970240462e5c5d4427915c702558d
Jonathan <jacksawyerwatx@users.noreply.github.com>
2025-04-08 19:57:30 -0500

    authentication implimentation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { ShoppingCart, LogOut, Lock, LogIn, UserPlus } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useUserStore } from "../stores/useUserStore";
 
 const Navbar = () => {
-    const user = true;
-    const isAdmin = true;
+    const { user, logout } = useUserStore();
+    const isAdmin = user?.role === "admin";
 
     return (
         <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
@@ -26,19 +27,20 @@ const Navbar = () => {
                     </Link>
                     )}
                     {isAdmin && (
-                        <Link className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium transition duration-300 ease-in-out flex items-center">
+                        <Link className="bg-emerald-700 hover:bg-emerald-500 text-white px-3 py-1 rounded-md font-medium transition duration-300 ease-in-out flex items-center">
                             <Lock className="inline-block mr-1" size={18} />
                             <span classname="hidden sm:inline">Dashboard</span>
                         </Link>
                     )}
                     {user ? (
-                        <button className="bg-gray-700 hover:bg-grey-600 text-white py-2 px-4 rounded-md flex items-center transition duration-300 ease-in-out">
+                        <button className="bg-gray-700 hover:bg-gray-500 text-white px-4 py-1 rounded-md flex items-center transition duration-300 ease-in-out"
+                            onClick={logout} >
                             <LogOut size={18} />
                             <span className="hidden sm:inline ml-2">Logout</span>
                         </button>
                     ) : (
                         <>
-                            <Link to={"/signup"} className="bg-emrald-600 hover:bg-emerald-700 text-white py-2 px-4 rounded-md flex items-center transition duration-300 ease-in-out">
+                            <Link to={"/signup"} className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 rounded-md flex items-center transition duration-300 ease-in-out">
                                 <UserPlus className="mr-2" size={18} />
                                 Sign Up
                             </Link>
