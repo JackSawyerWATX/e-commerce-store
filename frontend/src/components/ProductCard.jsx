564fcc74e46be8af2c8a6231178e0eafb5a12b75
Jonathan <jacksawyerwatx@users.noreply.github.com>
2025-04-21 19:56:10 -0500

    building cart page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,20 +1,20 @@
 import toast from "react-hot-toast"
 import { ShoppingCart } from "lucide-react"
 import { useUserStore } from "../stores/useUserStore"
-
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductCard = ({ product }) => {
     const { user } = useUserStore();
+    const { addToCart } = useCartStore();
 
     const handleAddToCart = () => {
-        if(!user) {
-            toast.error("Please log in to add products to cart.", {id: "login"});
+        if (!user) {
+            toast.error("Please log in to add products to cart.", { id: "login" });
             return
         } else {
-            toast.success("Product added to cart: ", product);
-            return;
+            addToCart(product);
+            toast.success("Product added to cart.", { id: "addToCart" });
         }
-        toast.success("Product added to cart.");
     }
 
     return (
